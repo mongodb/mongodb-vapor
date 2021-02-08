f43ca84f095f7a648616caeb9cb15f6ee5c96cfc
Kaitlin Mahar <kaitlinmahar@gmail.com>
2021-02-08 18:24:14 -0500

    Use ByteBuffer APIs for extJSON encoding/decoding

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")),
         // TODO: update to driver v1.1 once released.
-        .package(url: "https://github.com/mongodb/mongo-swift-driver", .branch("master")),
+        .package(url: "https://github.com/mongodb/mongo-swift-driver", .branch("main")),
         .package(url: "https://github.com/vapor/vapor", .upToNextMajor(from: "4.0.0"))
     ],
     targets: [
