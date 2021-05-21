743c575684f28b5e267c767c15477c719eb86a3a
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2021-05-21 19:49:31 -0400

    Update MongoDBVapor.swift

--- Sources/MongoDBVapor/MongoDBVapor.swift
+++ Sources/MongoDBVapor/MongoDBVapor.swift
@@ -131,7 +131,7 @@ extension Application {
  * extension Request {
  *     /// A collection with an associated `Codable` type `Kitten`.
  *     var kittenCollection: MongoCollection<Kitten> {
- *         self.client.db("home").collection("kittens", withType: Kitten.self)
+ *         self.mongoDB.client.db("home").collection("kittens", withType: Kitten.self)
  *     }
  * }
  * ```
