669ce310520a99cf8cbddb2e1931b74d3ef3efe9
Kaitlin Mahar <kaitlinmahar@gmail.com>
2022-01-12 15:21:17 -0500

    Unneeded swiftlint disable

--- Sources/MongoDBVapor/MongoDBVaporVersion.swift
+++ Sources/MongoDBVapor/MongoDBVaporVersion.swift
@@ -1,5 +1,4 @@
 // Generated using Sourcery 1.6.0 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
-// swiftlint:disable:previous vertical_whitespace
 internal let MongoSwiftVersionString = "1.0.1"
