588e2f480abd4800f92a5c5f5674164063849dce
Kaitlin Mahar <kaitlinmahar@gmail.com>
2022-08-01 12:27:42 -0400

    1.1.0

--- Sources/MongoDBVapor/MongoDBVaporVersion.swift
+++ Sources/MongoDBVapor/MongoDBVaporVersion.swift
@@ -1,4 +1,4 @@
 // Generated using Sourcery 1.6.0 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
-internal let MongoDBVaporVersionString = "1.1.0-beta.1"
+internal let MongoDBVaporVersionString = "1.1.0"
